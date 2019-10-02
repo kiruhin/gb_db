b940a2ec32e1e9ccaa275912a76149a9f5995377
Kiruhin <ikiruhin@bk.ru>
2019-10-02 21:56:21 +0300

    webinar_1 code samples updated

--- webinar_1/code samples.sql
+++ webinar_1/code samples.sql
@@ -9,6 +9,7 @@ SHOW DATABASES;
 
 --Создадим БД и рассмотрим основные команды DDL
 
+-- команды DDL (data definition language)
 --Создадим базу данных
 CREATE DATABASE geekbrains;
 
@@ -49,3 +50,37 @@ DROP TABLE courses;
 
 --Удаляем базу данных
 DROP DATABASE geekbrains;
+
+
+-- команды DML (data manipulation language)
+-- CRUD
+-- вставка данных
+INSERT INTO courses (name) VALUES
+("Databases"), 
+("Linux"),
+("BigDATA");
+
+INSERT INTO students (firstname, lastname, email, couse_id) VALUES 
+("Pavel", "Durov", "ivanov@yandex.ru", "1"),
+("Bill", "Gates", "petrov@yandex.ru", "2"),
+("Ivan", "Grozny", "i.pushkina@yandex.ru", "3"),
+("Olga", "Buzova", "megastar@yandex.ru", "1")
+
+-- выборка данных
+SELECT * from courses;
+SELECT * from students
+where lastname = 'Gates';
+
+-- обновление данных
+update students
+set email = 'o.b@gmail.com'
+where lastname = 'Buzova';
+
+-- удаление данных
+delete from students
+where couse_id = 1;
+
+delete from students;
+
+
+
